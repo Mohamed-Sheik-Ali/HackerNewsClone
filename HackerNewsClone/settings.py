f5b1814f6826a129281509703dbaf4369452e657
Mohamed Sheik Ali <mohamed.sheik.ali1127@zohomail.in>
2021-09-10 12:25:36 +0530

    Update settings.py

--- HackerNewsClone/settings.py
+++ HackerNewsClone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bqy3eihu2&c7b34uplazwbxpi62lg0796z9dv4b123&ec02lv@'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
